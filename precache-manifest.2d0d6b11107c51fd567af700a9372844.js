2b03d5d28b2ddfe7d5ec0d43f19290b7e86431b1
ruby840124 <ruby840124@gmail.com>
2019-10-16 09:47:53 +0800

    Updates

---
+++ precache-manifest.2d0d6b11107c51fd567af700a9372844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bebcba85bce6def5a6a2c200429075c",
+    "url": "/webcam/index.html"
+  },
+  {
+    "revision": "41c8b15d0d514eae4de6",
+    "url": "/webcam/static/css/main.5f67dfb0.chunk.css"
+  },
+  {
+    "revision": "67b006ba8e71bb22705e",
+    "url": "/webcam/static/js/2.1945d3f8.chunk.js"
+  },
+  {
+    "revision": "41c8b15d0d514eae4de6",
+    "url": "/webcam/static/js/main.eea9d75a.chunk.js"
+  },
+  {
+    "revision": "6a89e5d8e6b6e16f78ac",
+    "url": "/webcam/static/js/runtime-main.80b3334b.js"
+  },
+  {
+    "revision": "547d3e276feb48f78a45803972219028",
+    "url": "/webcam/static/media/cat.547d3e27.jpg"
+  }
+]);
